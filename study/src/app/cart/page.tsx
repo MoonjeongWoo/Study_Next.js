bbde13496e5e13a7b618dceb96421b0f9374dc4b
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2023-09-03 16:31:06 +0900

    Update useState
    Update useState

--- study/src/app/cart/page.tsx
+++ study/src/app/cart/page.tsx
@@ -4,26 +4,50 @@ import age from "./DATA"
 import { HELLO } from "../abc/def/hello"
 
 export default function Cart() {
+
+  let 장바구니 =['Tamtoes','Pasta']
     return (
       <div>
         <h4 className="title">Cart</h4>
         <HELLO></HELLO>
-        <CART></CART>
-        <CART></CART>
+        <CART items ={장바구니[0]}></CART>
+        <CART items ={장바구니[1]}></CART>
+        <Banner items = "롯데"></Banner>
       </div>
     )
   }
 
-export function CART(){
+export function CART(props:any){
     return(
         <div className="cart-item">
-          <p>상품명 {age}</p>
+          <p>상품명 {props.items}</p>
           <p>$40</p>
           <p>1개</p>
+          <Button bgcolor="red"></Button>
         </div>
     )
   }
  
+
+export function Banner(props:any) {
+  return(
+    <div>
+      <h5>
+        {props.items}카드 결제 행사중
+      </h5>
+    </div>
+  )
+}
+
+export function Button(props : any){
+  return(
+    <div>
+      <button style={{background: props.bgcolor, color:"white"}}>
+        결제
+      </button>
+    </div>
+  )
+}
   //CSR => 자바스크립트 기능널기 불가능 하이드레이션 필요
   //SSR => 자바스크립트 기능넣기 가능 
 
--- study/src/app/list/page.tsx
+++ study/src/app/list/page.tsx
@@ -1,21 +1,33 @@
+;'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 
 
-export default function list() {
+export default function List() {
     let products = ['토마토','파스타','코코넛'];
-    
+     
+    const [count, setCount] = useState(0);
+
+
     return(
         <div>
             {
                 products.map((x,index)=>{
                     return(
+                        <>
                         <div className="food" key={index}>
                             <h4 >
                             {x}
                             </h4>
                             <Image src={`/food${index}.png`} width={500} height={500} alt={""}></Image>
+                        <span> {count} </span>
+                        <button onClick={()=>{
+                            setCount(count+1)
+                        }}>+</button>
                         </div>
+                        </>
+                        
                     )
                 })
             }
